7e7bb3c66b2962eaeae9696e8a8a442de644031b
jhermann <jh@web.de>
2025-06-09 20:13:15 +0200

    add support for 'Toppings'

--- linkdocs.sh
+++ linkdocs.sh
@@ -9,7 +9,17 @@ while read dir; do
     name=$(basename "$dir")
     letter=$(tr a-z A-Z <<<"${name:0:1}")
     mkdir -p "docs/$letter"
-    ln -nfs "../../$dir/README.md" "docs/$letter/$name.md"
+
+    if [ "$name" = "Toppings" ]; then
+        cp "$dir/README.md" "docs/$letter/$name.md"
+
+        ( ls -1 "$dir"/recipe-*.md 2>/dev/null || : ) | sort | \
+        while read include; do
+            cat "$include" >>"docs/$letter/$name.md"
+        done
+    else
+        ln -nfs "../../$dir/README.md" "docs/$letter/$name.md"
+    fi
 
     ( ls -1 "$dir"/*.{png,jpg,jpeg} 2>/dev/null || : ) | \
     while read img; do
