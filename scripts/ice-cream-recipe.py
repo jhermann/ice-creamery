ffddb169e4ac6b1d5e8a81973453e747c7ac651a
jhermann <jh@web.de>
2025-07-18 12:52:24 +0200

    add Flavor Powder as a stop word

--- scripts/ice-cream-recipe.py
+++ scripts/ice-cream-recipe.py
@@ -127,7 +127,7 @@ AUTO_LINK_STOP_WORDS = {
     'Bean Powder', 'berry powder', 'Root powder', 'cream-filled',
     'Cherries, Sour', 'Condensed milk', 'Cream 32%', 'Cream Punch',
     'desiccated', 'Evaporated milk', 'Extract',
-    'Flavor drops', 'Irish Cream', 'Low fat milk', 'Milk 3.5%',
+    'Flavor drops', 'Flavor Powder', 'Irish Cream', 'Low fat milk', 'Milk 3.5%',
     'Peanut butter', 'Philadelphia Milka',
     'Soy sauce',
 }
